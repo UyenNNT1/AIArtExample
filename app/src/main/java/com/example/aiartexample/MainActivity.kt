490487467e46231c598403790cc16323e46c39cb
UyenNNT1 <uyenntt1@apero.vn>
2025-06-09 10:09:14 +0700

    first commit

--- app/src/main/java/com/example/aiartexample/MainActivity.kt
+++ app/src/main/java/com/example/aiartexample/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello em $name!",
         modifier = modifier
     )
 }
